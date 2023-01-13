9017b79e82f652b5d198e32697f5bc2c1592eab2
trungnguyendev <trungvjppro23@gmail.com>
2023-01-13 16:07:46 +0700

    5

--- src/componets/Header/Header.js
+++ src/componets/Header/Header.js
@@ -21,11 +21,13 @@ const Header = () => {
                         <Nav.Link href="/admin">Admin</Nav.Link> */}
                     </Nav>
                     <Nav>
-                        <NavDropdown title="Settings" id="basic-nav-dropdown">
+                        <button className='btn-login'>Log In</button>
+                        <button className='btn-signup'>Sign Up</button>
+                        {/* <NavDropdown title="Settings" id="basic-nav-dropdown">
                             <NavDropdown.Item >Log in</NavDropdown.Item>
                             <NavDropdown.Item >Log out</NavDropdown.Item>
                             <NavDropdown.Item >Profile</NavDropdown.Item>
-                        </NavDropdown>
+                        </NavDropdown> */}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
