83d15725fce0dc5b48b2fbe946807b76cfa0ca52
trungnguyendev <trungvjppro23@gmail.com>
2023-01-20 23:04:40 +0700

    16

--- src/componets/Auth/Login.js
+++ src/componets/Auth/Login.js
@@ -3,10 +3,13 @@ import './Login.scss'
 import { useNavigate } from 'react-router-dom';
 import { postLogin } from "../../services/apiService"
 import { toast } from 'react-toastify';
+import { useDispatch } from 'react-redux';
+import { doLogin } from '../../redux/action/userAction';
 const Login = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const navigate = useNavigate()
+    const dispatch = useDispatch();
     const handleBackHome = () => {
         navigate('/')
     }
@@ -16,6 +19,7 @@ const Login = () => {
     const handleLogin = async () => {
         let data = await postLogin(email, password)
         if (data && +data.EC === 0) {
+            dispatch(doLogin(data))
             toast.success(data.EM)
             navigate('/')
         }
