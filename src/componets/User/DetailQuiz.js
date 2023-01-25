1a0871007d2bd1c40db23b652e89b9e954b4e482
trungnguyendev <trungvjppro23@gmail.com>
2023-01-25 10:27:52 +0700

    e

--- src/componets/User/DetailQuiz.js
+++ src/componets/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { getDataQuiz } from "../../services/apiService"
+import _ from 'lodash'
 const DetailQuiz = (props) => {
     const params = useParams()
     const quizId = params.id
@@ -9,7 +10,25 @@ const DetailQuiz = (props) => {
     }, [quizId])
     const fetchQuestions = async () => {
         let res = await getDataQuiz(quizId)
-        console.log(res)
+        if (res && res.EC === 0) {
+            let raw = res.DT
+            let data = _.chain(raw)
+                .groupBy("id")
+                .map((value, key) => {
+                    let questionDescription, image = null;
+                    let answers = []
+                    value.forEach((item, index) => {
+                        answers.push(item.answers)
+
+                        if (index === 0) {
+                            questionDescription = item.description
+                            image = item.image
+                        }
+                    })
+                    return { questionId: key, answers: answers, questionDescription, image }
+                })
+                .value()
+        }
     }
     return (
         <div className="detail-quiz-container">
